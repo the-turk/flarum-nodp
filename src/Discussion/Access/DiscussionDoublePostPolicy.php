d74708f6ada4ac89b542d644bad7e21449355295
William <nearata@protonmail.com>
2023-03-20 21:57:13 +0100

    revert back
    * i may have missed something

--- src/Discussion/Access/DiscussionDoublePostPolicy.php
+++ src/Discussion/Access/DiscussionDoublePostPolicy.php
@@ -4,6 +4,7 @@ namespace TheTurk\NoDP\Discussion\Access;
 
 use Flarum\Discussion\Discussion;
 use Flarum\Post\CommentPost;
+use Flarum\Post\Post;
 use Flarum\Settings\SettingsRepositoryInterface;
 use Flarum\User\Access\AbstractPolicy;
 use Flarum\User\User;
@@ -25,42 +26,46 @@ class DiscussionDoublePostPolicy extends AbstractPolicy
 
     protected function doublePost(User $user, Discussion $discussion)
     {
-        return $user->hasPermission('discussion.doublePost')
-            || $this->canDoublePost($user, $discussion);
+        return $this->canDoublePost($user, $discussion);
     }
 
-    private function canDoublePost(User $user, Discussion $discussion): bool
+    private function canDoublePost(User $actor, Discussion $discussion): bool
     {
         $lastPost = $discussion->lastPost;
 
-        if (!($lastPost instanceof CommentPost)) return true;
+        if ($lastPost instanceof CommentPost) {
+            if ($actor->hasPermission('discussion.doublePost')) return true;
 
-        /**
-         * Prevent users from by-passing the double-posting check
-         * by soft-deleting and restoring their previous post.
-         * 
-         * @var \Flarum\Post\Post
-         */
-        $hiddenPost = $discussion->posts
-            ->where('user_id', $user->id)
-            ->where('created_at', '>=', $discussion->last_posted_at)
-            ->whereNotNull('hidden_at')
-            ->whereColumn('hidden_user_id', 'user_id')
-            ->orderBy('hidden_at', 'desc')
-            ->first();
+            /**
+             * Prevent users from by-passing the double-posting check
+             * by soft-deleting and restoring their previous post.
+             * 
+             * @var ?Post
+             */
+            $hiddenLastPost = Post::where('user_id', $actor->id)
+                ->where('discussion_id', $lastPost->discussion->id)
+                ->where('created_at', '>=', $lastPost->created_at)
+                ->whereNotNull('hidden_at')
+                ->whereColumn('hidden_user_id', 'user_id')
+                ->orderBy('hidden_at', 'desc')
+                ->first();
 
-        if (!is_null($hiddenPost)) {
-            $lastPost = $hiddenPost;
+            if (!is_null($hiddenLastPost)) {
+                $lastPost = $hiddenLastPost;
+            }
+
+            // what is this ?!
+            // if ($actor->cannot('edit', $lastPost)) return true;
+            
+            if ($actor->id == $lastPost->user_id) {
+                $timeLimit = $this->settings->get('the-turk-nodp.time_limit');
+
+                $isExpired = $lastPost->created_at->addMinutes($timeLimit)->isPast();
+
+                if ($timeLimit == 0 || !$isExpired) return false;
+            }
         }
 
-        if ($lastPost->user_id != $user->id) return true;
-
-        if ($user->cannot('edit', $lastPost)) return false;
-
-        $timeLimit = (int) $this->settings->get('the-turk-nodp.time_limit');
-
-        if ($timeLimit == 0) return false;
-
-        return $lastPost->created_at->addMinutes($timeLimit)->isPast();
+        return true;
     }
 }
